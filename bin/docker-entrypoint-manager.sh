9f58d24dde5599972d453db53f21ac16571f5f66
David Moles <dmoles@berkeley.edu>
2020-02-19 10:46:32 -0800

    Add comment re: credentials & manager

--- bin/docker-entrypoint-manager.sh
+++ bin/docker-entrypoint-manager.sh
@@ -9,6 +9,11 @@ WMSMGR_HOME="${WMSAPP_HOME}/manager"
 # ########################################
 # Manager/API credentials
 
+# Note that we don't really need these, but if they're
+# not set, the server isn't going to start, and if it
+# doesn't and we do, it's just going to keep restarting
+# perpetually
+
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 # shellcheck source=manager-credentials.sh
 source "${DIR}/manager-credentials.sh"
